36fef9a709f5641120b37c68ad46201667e63109
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-04-09 15:13:36 +0200

    feat: add  Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from 'react'
-import articles from './data/articles'
+//import articles from './data/articles'
 
 function App() {
 
-  console.log(articles)
+  const [articleTitle, setArticleTitle] = useState([
+    "Le borse sono tornate in forte calo, dopo i nuovi dazi di Trump",
+    "Cosa fare per ridurre gli effetti della fast fashion",
+    "Come funzionano le sanzioni per le bestemmie nel calcio italiano",
+    "In Myanmar far arrivare i soccorsi dopo il terremoto è ancora un enorme problema",
+    "Raggiunto l accordo di governo in Germania tra Cdu e Spd. Merz sarà cancelliere",
+    "Frosinone, il bus di una scolaresca tampona un camion: 41 bambini coinvolti, 4 portati in ospedale"
+  ]);
+  const [newArticleTitle, setNewArticleTitle] = useState('')
+
+  const addNewArticleTitle = event => {
+    event.preventDefault();
+    setArticleTitle([...articleTitle, newArticleTitle])
+  }
+
 
   return (
     <>
@@ -12,9 +26,16 @@ function App() {
       </div>
       <div>
         <ul>
-          {articles.map(article => <li key={article.id}>{article.title}</li>)}
+          {articleTitle.map((article, i) => <li key={i}>{article}</li>)}
         </ul>
 
+        <form onSubmit={addNewArticleTitle}>
+          <input
+            type="text"
+            value={newArticleTitle}
+            onChange={e => { setNewArticleTitle(e.target.value) }}
+          ></input><button>Aggiungi</button>
+        </form>
       </div>
     </>
 
