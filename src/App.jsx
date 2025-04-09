d8971459c5316b3fde37c50b89b3b08f975d8aad
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-04-09 13:52:29 +0200

    feat: add Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
-
+import articles from './data/articles'
 
 function App() {
 
+  console.log(articles)
 
   return (
     <>
+      <div>
+        <h1>Lista articoli di giornale</h1>
+      </div>
+      <div>
+        <ul>
+          {articles.map(article => <li key={article.id}>{article.title}</li>)}
+        </ul>
 
+      </div>
     </>
 
   )
---
+++ src/data/articles.js
@@ -0,0 +1,28 @@
+const articles = [
+    {
+        id: "1",
+        title: "Le borse sono tornate in forte calo, dopo i nuovi dazi di Trump"
+    },
+    {
+        id: "2",
+        title: "Cosa fare per ridurre gli effetti della fast fashion"
+    },
+    {
+        id: "3",
+        title: "Come funzionano le sanzioni per le bestemmie nel calcio italiano"
+    },
+    {
+        id: "4",
+        title: "In Myanmar far arrivare i soccorsi dopo il terremoto è ancora un enorme problema"
+    },
+    {
+        id: "5",
+        title: "Raggiunto l accordo di governo in Germania tra Cdu e Spd. Merz sarà cancelliere"
+    },
+    {
+        id: "6",
+        title: "Frosinone, il bus di una scolaresca tampona un camion: 41 bambini coinvolti, 4 portati in ospedale"
+    },
+]
+
+export default articles
